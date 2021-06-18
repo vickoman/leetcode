9d1ca200da279b38325c354f81826c7e5f871f7d
vickoman <vicmandlagasca@gmail.com>
2021-06-18 18:00:43 -0500

    Add solution in c++ of reverse string

---
+++ reverse-string/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int a=0;
+        int b = s.size() - 1;
+        
+        while(a<=b) {
+            swap(s[a], s[b]);
+            a++;
+            b--;
+        }
+    }
+};
