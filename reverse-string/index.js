ea754f5444a3eaeab03ae08de80304260e3a50b4
Victor Diaz <victor@onecommons.org>
2021-06-18 10:38:38 -0500

    Add Reverse string  solution in javascript

---
+++ reverse-string/index.js
@@ -0,0 +1,16 @@
+const reverse = (str) => {
+    let arrStr = str.split("");
+    let a = 0;
+    let b = arrStr.length - 1;
+
+    while(a<b) {
+        let tmp = arrStr[a];
+        arrStr[a] = arrStr[b];
+        arrStr[b] = tmp;
+        a += 1;
+        b -= 1;
+    }
+    return arrStr.join("");
+};
+
+console.log(reverse("vickoman")); //should return namokciv
